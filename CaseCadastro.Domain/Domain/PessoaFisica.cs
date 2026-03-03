0fbf48edf4e4bbd208337b3245e7f6c705b3aa5f
guilherme.yasuda <guilherme.yasuda@SL287>
2026-03-03 19:15:17 -0300

    feat(domain): criação das entidades PessoaFisica, PessoaJuridica e Endereco

---
+++ CaseCadastro.Domain/Domain/PessoaFisica.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseCadastro.Domain.Domain
+{
+    public class PessoaFisica
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public Endereco Endereco { get; set; }
+    }
+}
---
+++ CaseCadastro.Domain/Domain/PessoaJuridica.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseCadastro.Domain.Domain
+{
+    public class PessoaJuridica
+    {
+        public Guid Id { get; set; }
+        public string RazaoSocial { get; set; }
+        public string Cnpj { get; set; }
+        public Endereco Endereco { get; set; }
+    }
+}
